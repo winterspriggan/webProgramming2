3fa5d2ada7293affcdd14977cf6a034be8a96434
winterspriggan <jin-_-x@naver.com>
2023-11-21 19:11:20 +0900

    sql문 추가

---
+++ database/ddinggeunmarket.sql
@@ -0,0 +1,19 @@
+CREATE USER dding1 IDENTIFIED BY 1234;
+
+GRANT CREATE SESSION TO dding;
+GRANT ALTER SESSION TO dding;
+GRANT CREATE TABLE TO dding;
+GRANT CREATE ANY TABLE TO dding;
+GRANT CREATE TABLESAPCE TO dding;
+GRANT UNLIMITED TABLESPACE TO dding;
+
+
+CREATE TABLE users2 (
+                        id VARCHAR2(255) PRIMARY KEY NOT NULL,
+                        phoneNumb VARCHAR2(255) NOT NULL,
+                        password VARCHAR2(255) NOT NULL,
+                        dob VARCHAR2(255) NOT NULL,
+                        email VARCHAR2(255) NOT NULL,
+                        address VARCHAR2(255) NOT NULL,
+                        name VARCHAR2(255) NOT NULL
+);
