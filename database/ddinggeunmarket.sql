854c9a91c687b8927f1d153be57aaaec435d5a4c
scn05033 <116643726+scn05033@users.noreply.github.com>
2023-12-09 18:22:26 +0900

    Update ddinggeunmarket.sql

--- database/ddinggeunmarket.sql
+++ database/ddinggeunmarket.sql
@@ -17,3 +17,30 @@ CREATE TABLE users2 (
                         address VARCHAR2(255) NOT NULL,
                         name VARCHAR2(255) NOT NULL
 );
+create table board (
+  board_idx number(4) primary key,
+  board_name varchar2(20),
+  board_title varchar2(100),
+  board_content varchar2(300),
+  board_date date default sysdate,
+  board_hit number(4) default 0,
+  board_likes number(4) default 0
+  );
+  
+ create sequence board_seq;
+ create table reply (
+  reply_idx number(4) primary key,
+  reply_name varchar2(20),
+  reply_content varchar2(300),
+  reply_date date default sysdate,
+  reply_board_idx number(4)
+  );
+  create table sub_reply (
+  sub_reply_idx number(4) primary key,
+  sub_reply_name varchar2(20),
+  sub_reply_content varchar2(300),
+  sub_reply_date date default sysdate,
+  sub_reply_reply_idx number(4)
+  );
+ create sequence sub_reply_seq;
+ create sequence reply_board_seq;
