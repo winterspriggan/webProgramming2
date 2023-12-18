3bc8a3aa07fe5275e8860655fc3fa7620f760825
winterspriggan <jin-_-x@naver.com>
2023-12-18 17:33:18 +0900

    kakao login (2)

--- frontend/src/view/login/KakaoLogin.jsx
+++ frontend/src/view/login/KakaoLogin.jsx
@@ -1,120 +1,63 @@
-// import axios from "axios";
-// import swal from "sweetalert";
-// import React from "react";
-//
-// const CLIENT_ID = "092b8dd8c678497e53440f488cc95702";
-// const REDIRECT_URI = "http://localhost:40040/kakao/login";
-//
-// export const SocialKakao = () => {
-//     const handleLogin = () => {
-//         // 새로운 창 또는 팝업에서 Kakao 로그인 페이지로 이동
-//         const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
-//         const newWindow = window.open(kakaoURL, '_blank');
-//
-//         // Kakao OAuth 인증코드를 얻는 로직
-//         const handleCode = (event) => {
-//             if (event.source === newWindow) {
-//                 // 팝업이 닫히면 이벤트 리스너를 제거하고 인증코드를 사용할 수 있음
-//                 window.removeEventListener('message', handleCode);
-//                 const code = event.data;
-//                 console.log('Kakao OAuth 인증코드:', code);
-//
-//                 // 백엔드로 인가 코드 전송
-//                 axios
-//                     .get('http://localhost:40040/kakao/login', {
-//                         params: {
-//                             code: code
-//                         },
-//                     })
-//                     .then(response => {
-//                         if (response.data !== '')
-//                             swal({
-//                                 title: '로그인 성공',
-//                                 text: '환영합니다, '  ,
-//                                 icon: 'success',
-//                                 button: '확인',
-//                             })
-//                         console.log('백엔드에서 받은 응답:', response.data)
-//                         // 여기서 얻은 엑세스 토큰을 사용할 수 있음
-//                         window.close();
-//                     })
-//                     .catch(error => {
-//                         console.error('백엔드 요청 중 오류:', error);
-//                     });
-//             }
-//         };
-//
-//         // Kakao OAuth 인증코드를 얻기 위한 이벤트 리스너 등록
-//         window.addEventListener('message', handleCode);
-//     };
-//
-//     return (
-//         <>
-//             <button onClick={handleLogin}>카카오 로그인</button>
-//         </>
-//     );
-// };
-
 import axios from "axios";
 import swal from "sweetalert";
-import React, {useState} from "react";
-import ExternalHtmlComponent from "../main/Main";
-import DdinggeunMarket from "../DdinggeunMarket";
-import {useNavigate} from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const CLIENT_ID = "092b8dd8c678497e53440f488cc95702";
 const REDIRECT_URI = "http://localhost:40040/kakao/login";
 
 export const SocialKakao = ({ setAuthenticated, setUser }) => {
-
     const navigate = useNavigate();
 
     const handleLogin = () => {
-        const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
-        window.location.href = kakaoURL;
+        const kakaoURL = `/kakao/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+        navigate(kakaoURL);
     };
 
     React.useEffect(() => {
-
         const handleCode = async () => {
-            const code = new URL(window.location.href).searchParams.get("code");
+            const urlSearchParams = new URLSearchParams(window.location.search);
+            const code = urlSearchParams.get("code");
+
             if (code) {
                 try {
-                    const response = await axios.get('http://localhost:40040/kakao/login')
-                .then((response) => {
-                        if (response.data !== '') {
-                            swal({
-                                title: '로그인 성공',
-                                text: '환영합니다!',
-                                icon: 'success',
-                                button: '확인',
-                            });
-                            setUser({
-                                id: response.data.id,
-                                name: response.data.name,
-                            });
-                            setAuthenticated(true);
-                            window.location.href = "http://localhost:40040";
-                        }
-
-                        console.log('백엔드에서 받은 응답:', response.data);
-                    })
+                    const response = await axios.get(`http://localhost:40040/kakao/login?code=${code}`);
+                    if (response.data !== "") {
+                        swal({
+                            title: "로그인 성공",
+                            text: "환영합니다!",
+                            icon: "success",
+                            button: "확인",
+                        });
+                        setUser({
+                            id: response.data.id,
+                            name: response.data.name,
+                        });
+                        setAuthenticated(true);
+                        navigate('/');
+                    }
+                    console.log('백엔드에서 받은 응답:', response.data);
                 } catch (error) {
                     console.error('백엔드 요청 중 오류:', error);
                 }
             }
         };
 
-        window.addEventListener('load', handleCode);
-        //
-        // return () => {
-        //     window.removeEventListener('load', handleCode);
-        // };
+
+
+        window.addEventListener("load", handleCode);
+
+        return () => {
+            window.removeEventListener("load", handleCode);
+        };
     }, []);
 
     return (
         <>
-            <button onClick={handleLogin} style={{ background: 'none', border: 'none', padding: 0, cursor: 'pointer' }}>
+            <button
+                onClick={handleLogin}
+                style={{ background: "none", border: "none", padding: 0, cursor: "pointer" }}
+            >
                 <img src="/kakao_login.png" />
             </button>
         </>
