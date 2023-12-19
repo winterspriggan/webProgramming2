0559b88630abd7a91d8326af78696b49cee85982
junyoungss <128023213+junyoungss@users.noreply.github.com>
2023-12-19 21:26:05 +0900

    Add files via upload

---
+++ src/main/java/ddinggeunmarket_service/jpa/chat/ChatMessage.java
@@ -0,0 +1,23 @@
+package ddinggeunmarket_service.jpa.chat;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatMessage {
+    public enum MessageType {
+        ENTER, TALK
+    }
+    private MessageType type;
+    //채팅방 ID
+    private String roomId;
+    //보내는 사람
+    private String sender;
+    //내용
+    private String message;
+}
