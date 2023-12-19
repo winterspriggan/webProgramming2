331cdf7b52d190f8ff658d6750be4340b33e0f08
junyoungss <128023213+junyoungss@users.noreply.github.com>
2023-12-19 21:27:03 +0900

    Add files via upload

---
+++ src/main/java/ddinggeunmarket_service/service/ChatService.java
@@ -0,0 +1,41 @@
+package ddinggeunmarket_service.service;
+
+import ddinggeunmarket_service.jpa.chat.ChatRoom;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.InitializingBean;
+import java.util.*;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ChatService implements InitializingBean{
+    private Map<String, ChatRoom> chatRooms;
+
+    //의존관게 주입완료되면 실행되는 코드
+    public void afterPropertiesSet() {
+        chatRooms = new LinkedHashMap<>();
+    }
+
+    //채팅방 불러오기
+    public List<ChatRoom> findAllRoom() {
+        //채팅방 최근 생성 순으로 반환
+        List<ChatRoom> result = new ArrayList<>(chatRooms.values());
+        Collections.reverse(result);
+
+        return result;
+    }
+
+    //채팅방 하나 불러오기
+    public ChatRoom findById(String roomId) {
+        return chatRooms.get(roomId);
+    }
+
+    //채팅방 생성
+    public ChatRoom createRoom(String name) {
+        ChatRoom chatRoom = ChatRoom.create(name);
+        chatRooms.put(chatRoom.getRoomId(), chatRoom);
+        return chatRoom;
+    }
+}
